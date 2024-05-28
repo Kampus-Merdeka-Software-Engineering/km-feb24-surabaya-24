7022946e8df723607be22e635c7e555564fa23bd
KevinCandra9999 <candrakevin14@gmail.com>
2024-05-28 12:24:02 +0700

    up
    baru 2 graph

--- JS/skripnya.js
+++ JS/skripnya.js
@@ -45,3 +45,103 @@ function filterTable() {
   var selectedMonthYear = document.getElementById("monthYearSelect").value;
   alert("You selected: " + selectedMonthYear); // Replace this with your logic
 }
+
+/* table penjualan */
+fetch("table_penjualan.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (products) {
+    products = products.slice(0, 10);
+
+    let placeholder = document.querySelector("#data-output");
+    let out = "";
+    for (let product of products) {
+      out += `
+        <tr>
+          <td>${product.Name}</td>
+          <td>${product.Size}</td>
+          <td>${product.Category}</td>
+          <td>${product.Price}</td>
+          <td>${product.Order_vol}</td>
+        </tr>
+      `;
+    }
+    placeholder.innerHTML = out;
+  });
+
+/* time barchart */
+const ctx = document.getElementById("timebarchart");
+
+fetch("time_of_pizza_sales.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    console.log(data);
+    createchart(data, "bar");
+  });
+
+function createchart(data, type) {
+  new Chart(ctx, {
+    type: type,
+    data: {
+      labels: data.map((row) => row.Times),
+      datasets: [
+        {
+          label: "Record Count",
+          data: data.map((row) => row.Record_Count),
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
+
+/* revenue linechart */
+const ctx2 = document.getElementById("revenue");
+
+fetch("revenue.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
+    }
+  })
+  .then(function (data2) {
+    console.log(data2);
+    createchart2(data2, "line");
+  });
+
+function createchart2(data2, type2) {
+  new Chart(ctx2, {
+    type: type2,
+    data: {
+      labels: data2.map((row) => row.Bulan_Tahun),
+      datasets: [
+        {
+          label: "price",
+          data: data2.map((row) => row.price),
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
+
+/* price range barchart*/
